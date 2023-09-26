0f2b151ca0c58bd4d9393be3e9e6bfe326c54ac8
abc <abc@gmail.com>
2023-09-26 11:13:07 +0000

    finally completed

---
+++ java-programs-phase1/src/javaprograms/whileloop.java
@@ -0,0 +1,13 @@
+package javaprograms;
+
+public class whileloop {
+	
+public static void main(String[] args) {
+int i=1;
+while(i<10);{
+i++;
+System.out.println("i");
+}
+System.out.println("printed values from 1 t0 10");
+}
+}
